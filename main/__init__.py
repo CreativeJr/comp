1aea67b2f63b5bd5208a6ade0495030c5230167c
CreativeJr <73512563+CreativeJr@users.noreply.github.com>
2022-12-15 03:54:53 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -7,9 +7,9 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-BOT_UN = config("BOT_UN", default=None)
+API_ID = config("API_ID", 4926633)
+API_HASH = config("API_HASH", default="be7b1d39ac5116d22701f8b6ac956785")
+BOT_TOKEN = config("BOT_TOKEN", default="be7b1d39ac5116d22701f8b6ac956785")
+BOT_UN = config("BOT_UN", default="@Privatecompressor_bot")
 
-Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+Drone = TelegramClient('bot', 4926633, "be7b1d39ac5116d22701f8b6ac956785").start(bot_token="be7b1d39ac5116d22701f8b6ac956785") 
